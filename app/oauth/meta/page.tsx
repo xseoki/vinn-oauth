fe7abf8eb7836bf03108c0598f8de4f5985df238
ourworks <os744546@gmail.com>
2026-04-10 19:50:35 +0900

    fix: simplify oauth page

--- app/oauth/meta/page.tsx
+++ app/oauth/meta/page.tsx
@@ -1,9 +1,10 @@
+'use client'
+
 export default function MetaOAuthCallback() {
   return (
     <html>
       <body>
         <p>로그인 완료. 이 창은 자동으로 닫힙니다.</p>
-        <script dangerouslySetInnerHTML={{ __html: `window.close()` }} />
       </body>
     </html>
   )
